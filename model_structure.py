6c581f2c5c4d06c859a5a2d31ae76c66c9ec6f0a
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2023-01-17 14:53:47 +0100

    Update model_structure.py

--- model_structure.py
+++ model_structure.py
@@ -10,7 +10,6 @@ logging.basicConfig(
 )
 
 def activation_block(x):
-    #x = custom_gelu(x)
     x = ReLU()(x)
     return BatchNormalization()(x)
 
@@ -24,24 +23,11 @@ def conv_mixer_block(x, filters: int, kernel_size: int):
     x = Conv2D(filters, kernel_size=1, kernel_initializer='he_normal')(x)
     x = activation_block(x)
     return x
-  
+    
 
-def bi_fusion(input1, input2):
-    '''
-    input1: input tensor from the 2D network (x,y,channels)
-    input2: input tensor from the 3D network (x,y,depth,channels)
-    channel: channel number of the result
-    return: processed tensor
-    '''
-    channel = input1.get_shape().as_list()[-1]
-    conv2 = ReLU()(BatchNormalization()(Conv3D(1, 1, padding='same', kernel_initializer='he_normal')(input2)))
-    conv2 = tf.keras.backend.squeeze(conv2, axis=-1)
-    conv2 = ReLU()(BatchNormalization()(Conv2D(channel, 3, padding='same', kernel_initializer='he_normal')(conv2)))
-    conv2 = squeeze_excite_block(conv2)
-
-    conv1 = squeeze_excite_block(input1)
-
-    return Add()([conv1, conv2])
+def conv_stem(x, filters: int, patch_size: int):
+    x = layers.Conv2D(filters, kernel_size=patch_size, strides=patch_size)(x)
+    return activation_block(x)
   
   
 def cbam_block(cbam_feature, ratio=8):
@@ -114,59 +100,48 @@ def DilatedSpatialConv(dspp_input, filters=32):
     return x
   
 
-def AttUnet3d(input_size1=(160, 160, 1), input_size2=(160, 160, 1),
-         input_size3=(160, 160, 1), n_filt=32):
+def AttUnet2d(input_size1=(160, 160, 1), n_filt=32):
 
     input_model1 = Input(input_size1)
-    input_model2 = Input(input_size2)
-    input_model3 = Input(input_size3)
 
-    # layer1 2D
-    x1 = DilatedSpatialConv(input_model1, filters=n_filt)
-    conv1 = conv_mixer_block(x1, filters=n_filt, kernel_size=3)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    # layer1 3D
-    input_model3d = Concatenate(axis=-1)([input_model2, input_model1, input_model3])
-    input_model3d = tf.expand_dims(input_model3d, -1)
-    x1_2 = ReLU()(
-        BatchNormalization()(Conv3D(n_filt, 3, padding='same', kernel_initializer='he_normal')(input_model3d)))
-    conv1_2 = ReLU()(BatchNormalization()(Conv3D(n_filt, 3, padding='same', kernel_initializer='he_normal')(x1_2)))
-    pool1_2 = MaxPooling3D(pool_size=(2, 2, 1))(conv1_2)
+    # ConvMix layer 1
+    x1 = conv_stem(input_model1, filters=n_filt, patch_size=1)
+    for _ in range(8):
+        x1 = conv_mixer_block(x1, filters=n_filt, kernel_size=9)
+    
+    # Conv Layer 1
+    x12 = BatchNormalization()(ReLU()(Conv2D(n_filt, 3, padding='same', kernel_initializer='he_normal')(input_model1d)))
+    x12 = BatchNormalization()(ReLU()(Conv2D(n_filt, 3, padding='same', kernel_initializer='he_normal')(x12)))
+    
+    pool1 = MaxPooling2D(pool_size=(2, 2))(Add()([x1, x12]))
 
-    # layer2 2D
-    conv2 = conv_mixer_block(pool1, filters=n_filt * 2, kernel_size=3)
-    conv2 = conv_mixer_block(conv2, filters=n_filt * 2, kernel_size=3)
-    # layer2 3D
-    conv2_2 = ReLU()(
-        BatchNormalization()(Conv3D(n_filt * 2, 3, padding='same', kernel_initializer='he_normal')(pool1_2)))
-    conv2_2 = ReLU()(
-        BatchNormalization()(Conv3D(n_filt * 2, 3, padding='same', kernel_initializer='he_normal')(conv2_2)))
-    pool2_2 = MaxPooling3D(pool_size=(2, 2, 1))(conv2_2)
+    # ConvMix layer 2
+    x2 = conv_stem(input_model1, filters=n_filt*2, patch_size=2)
+    for _ in range(8):
+        x2 = conv_mixer_block(x2, filters=n_filt*2, kernel_size=7)
+    
+    # Conv Layer 2
+    x22 = BatchNormalization()(ReLU()(Conv2D(n_filt*2, 3, padding='same', kernel_initializer='he_normal')(pool1)))
+    x22 = BatchNormalization()(ReLU()(Conv2D(n_filt*2, 3, padding='same', kernel_initializer='he_normal')(x22)))
+    
+    pool2 = MaxPooling2D(pool_size=(2, 2))(Add()([x2, x22]))
 
-    select1 = bi_fusion(conv2, conv2_2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(select1)
+    # layer3
+    x3 = BatchNormalization()(ReLU()(Conv2D(n_filt*4, 3, padding='same', kernel_initializer='he_normal')(pool2)))
+    x3 = BatchNormalization()(ReLU()(Conv2D(n_filt*4, 3, padding='same', kernel_initializer='he_normal')(x3)))
+    
+    pool3 = MaxPooling2D(pool_size=(2, 2))(x3)
 
-    # layer3 2D
-    conv3 = conv_mixer_block(pool2, filters=n_filt * 4, kernel_size=3)
-    conv3 = conv_mixer_block(conv3, filters=n_filt * 4, kernel_size=3)
-    # layer3 3D
-    conv3_2 = ReLU()(
-        BatchNormalization()(Conv3D(n_filt * 4, 3, padding='same', kernel_initializer='he_normal')(pool2_2)))
-    conv3_2 = ReLU()(
-        BatchNormalization()(Conv3D(n_filt * 4, 3, padding='same', kernel_initializer='he_normal')(conv3_2)))
+    # layer4
+    x4 = BatchNormalization()(ReLU()(Conv2D(n_filt*8, 3, padding='same', kernel_initializer='he_normal')(pool3)))
+    x4 = BatchNormalization()(ReLU()(Conv2D(n_filt*8, 3, padding='same', kernel_initializer='he_normal')(x4)))
+    
+    pool4 = MaxPooling2D(pool_size=(2, 2))(x4)
 
-    select2 = bi_fusion(conv3, conv3_2)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(select2)
-
-    # layer4 2D
-    conv4 = conv_mixer_block(pool3, filters=n_filt * 8, kernel_size=3)
-    conv4 = conv_mixer_block(conv4, filters=n_filt * 8, kernel_size=3)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
-
-    # layer5 2D
-    conv5 = conv_mixer_block(pool4, filters=n_filt * 16, kernel_size=3)
-    conv5 = spatial_attention(conv5)
-    conv5 = conv_mixer_block(conv5, filters=n_filt * 16, kernel_size=3)
+    # layer5
+    x5 = BatchNormalization()(ReLU()(Conv2D(n_filt*16, 3, padding='same', kernel_initializer='he_normal')(pool4)))
+    x5 = BatchNormalization()(ReLU()(Conv2D(n_filt*16, 3, padding='same', kernel_initializer='he_normal')(x5)))
+ 
 
     up4 = UpSampling2D(size=(2, 2))(conv5)
     skip4 = cbam_block(conv4)
