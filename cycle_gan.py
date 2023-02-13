0fced2aceaac437ea14a40e78eb355bcc5103245
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2023-02-13 15:35:56 +0100

    Update cycle_gan.py

--- cycle_gan.py
+++ cycle_gan.py
@@ -1,64 +1,40 @@
-# example of preparing the horses and zebra dataset
 from os import listdir
 from numpy import asarray
 from numpy import vstack
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
 from numpy import savez_compressed
+from numpy import load
+from matplotlib import pyplot
+import concatenation
+
 
 # load all images in a directory into memory
 def load_images(path, size=(256,256)):
-	data_list = list()
-	# enumerate filenames in directory, assume all are images
-	for filename in listdir(path):
-		# load and resize the image
-		pixels = load_img(path + filename, target_size=size)
-		# convert to numpy array
-		pixels = img_to_array(pixels)
-		# store
-		data_list.append(pixels)
-	return asarray(data_list)
+    data_list = []
+    data = h5py.File(path, 'r')
+    train_img = data['img_raw'][()]
+    for i in range(len(train_img)):
+	# resize the image
+	img = cv2.resize(train_img[i], (400, 400), interpolation = cv2.INTER_CUBIC)
+	# normalize the image
+	img = normalize_image(img))
+	# store
+	data_list.append(img)
+    # convert to numpy array
+    return asarray(data_list)
 
 # dataset path
-path = 'horse2zebra/'
-# load dataset A
+path = 'xxxxx'
+# load dataset A (artifacts)
 dataA1 = load_images(path + 'trainA/')
 dataAB = load_images(path + 'testA/')
-dataA = vstack((dataA1, dataAB))
 print('Loaded dataA: ', dataA.shape)
-# load dataset B
+# load dataset B (no artifacts)
 dataB1 = load_images(path + 'trainB/')
 dataB2 = load_images(path + 'testB/')
 dataB = vstack((dataB1, dataB2))
 print('Loaded dataB: ', dataB.shape)
-# save as compressed numpy array
-filename = 'horse2zebra_256.npz'
-savez_compressed(filename, dataA, dataB)
-print('Saved dataset: ', filename)
-
-# load and plot the prepared dataset
-from numpy import load
-from matplotlib import pyplot
-# load the dataset
-data = load('horse2zebra_256.npz')
-dataA, dataB = data['arr_0'], data['arr_1']
-print('Loaded: ', dataA.shape, dataB.shape)
-# plot source images
-n_samples = 3
-for i in range(n_samples):
-	pyplot.subplot(2, n_samples, 1 + i)
-	pyplot.axis('off')
-	pyplot.imshow(dataA[i].astype('uint8'))
-# plot target image
-for i in range(n_samples):
-	pyplot.subplot(2, n_samples, 1 + n_samples + i)
-	pyplot.axis('off')
-	pyplot.imshow(dataB[i].astype('uint8'))
-pyplot.show()
-
-
-
-
 
 # example of training a cyclegan on the horse2zebra dataset
 from random import random
